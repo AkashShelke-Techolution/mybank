c4573b06bbc20cc1f7d20b2ed20b39b49be62845
Your Name <akash.shelke@techolution.com>
2025-11-18 18:26:14 +0530

    test

--- transferMoney.c
+++ transferMoney.c
@@ -7,7 +7,7 @@
  */
 void transferMoney(double* balance, double* lastTransferAmount)
 {
-	//modified by akash
+	//modified by akash PROD
 	char accountName[MAX_LENGTH], accountNumber[MIN_LENGTH], bank[MAX_LENGTH];
 	double amount;
 
